0ae1840b499b19d42a482268a1c03085ddd9a57e
AlexQcs <531040924@qq.com>
2017-08-22 17:43:58 +0800

    系统工具类

---
+++ system/NetworkUtil.java
@@ -0,0 +1,262 @@
+package com.hc.posterccb.util.system;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * Created by alex on 2017/7/8.
+ */
+
+public class NetworkUtil {
+
+    public static int NET_CNNT_BAIDU_OK = 1; // NetworkAvailable
+    public static int NET_CNNT_BAIDU_TIMEOUT = 2; // no NetworkAvailable
+    public static int NET_NOT_PREPARE = 3; // Net no ready
+    public static int NET_ERROR = 4; //net error
+    private static int TIMEOUT = 3000; // TIMEOUT
+
+
+    /**
+     * check NetworkAvailable
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getApplicationContext().getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        if (null == manager)
+            return false;
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return  !(null == info || !info.isAvailable());
+    }
+
+    //获取本地IP
+    public static String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface
+                    .getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf
+                        .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("getLocalIpAddress", ex.toString());
+        }
+
+        return null;
+    }
+
+    /**
+     * 返回当前网络状态
+     *
+     * @param context
+     * @return
+     */
+    public static int getNetState(Context context) {
+        try {
+            ConnectivityManager connectivity = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivity != null) {
+                NetworkInfo networkinfo = connectivity.getActiveNetworkInfo();
+                if (networkinfo != null) {
+                    if (networkinfo.isAvailable() && networkinfo.isConnected()) {
+                        if (!connectionNetwork())
+                            return NET_CNNT_BAIDU_TIMEOUT;
+                        else
+                            return NET_CNNT_BAIDU_OK;
+                    } else {
+                        return NET_NOT_PREPARE;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return NET_ERROR;
+    }
+
+    /**
+     * ping "http://www.baidu.com"
+     *
+     * @return
+     */
+    static private boolean connectionNetwork() {
+        boolean result = false;
+        HttpURLConnection httpUrl = null;
+        try {
+            httpUrl = (HttpURLConnection) new URL("http://www.baidu.com")
+                    .openConnection();
+            httpUrl.setConnectTimeout(TIMEOUT);
+            httpUrl.connect();
+            result = true;
+        } catch (IOException e) {
+        } finally {
+            if (null != httpUrl) {
+                httpUrl.disconnect();
+            }
+            httpUrl = null;
+        }
+        return result;
+    }
+
+    /**
+     * check is3G
+     *
+     * @param context
+     * @return boolean
+     */
+    public static boolean is3G(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return  (activeNetInfo != null && activeNetInfo.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    /**
+     * isWifi
+     *
+     * @param context
+     * @return boolean
+     */
+    public static boolean isWifi(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return  (activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI) ;
+    }
+
+    /**
+     * is2G
+     *
+     * @param context
+     * @return boolean
+     */
+    public static boolean is2G(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        return  (activeNetInfo != null
+                && (activeNetInfo.getSubtype() == TelephonyManager.NETWORK_TYPE_EDGE
+                || activeNetInfo.getSubtype() == TelephonyManager.NETWORK_TYPE_GPRS || activeNetInfo
+                .getSubtype() == TelephonyManager.NETWORK_TYPE_CDMA)) ;
+    }
+
+    /**
+     * is wifi on
+     */
+    public static boolean isWifiEnabled(Context context) {
+        ConnectivityManager mgrConn = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        TelephonyManager mgrTel = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        return ((mgrConn.getActiveNetworkInfo() != null && mgrConn
+                .getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED) || mgrTel
+                .getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS);
+    }
+
+
+    /**
+     * Is the live streaming still available
+     * @return is the live streaming is available
+     */
+    public static boolean isLiveStreamingAvailable() {
+        // Todo: Please ask your app server, is the live streaming still available
+        return true;
+    }
+
+    public static String getLocalMacAddressFromIp() {
+        String mac_s= "";
+        try {
+            byte[] mac;
+            NetworkInterface ne=NetworkInterface.getByInetAddress(InetAddress.getByName(getLocalIpAddress()));
+            mac = ne.getHardwareAddress();
+            mac_s = byte2hex(mac);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mac_s.toUpperCase();
+    }
+
+    public static  String byte2hex(byte[] b) {
+        StringBuffer hs = new StringBuffer(b.length);
+        String stmp = "";
+        int len = b.length;
+        for (int n = 0; n < len; n++) {
+            stmp = Integer.toHexString(b[n] & 0xFF);
+            if (stmp.length() == 1)
+                hs = hs.append("0").append(stmp);
+            else {
+                hs = hs.append(stmp);
+            }
+            if (n!=len-1){
+                hs.append("-");
+            }
+        }
+        return String.valueOf(hs);
+    }
+
+    /**
+     * 得到有限网关的IP地址
+     *
+     * @return
+     */
+    public static String getLocalIp() {
+
+        try {
+            // 获取本地设备的所有网络接口
+            Enumeration<NetworkInterface> enumerationNi = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumerationNi.hasMoreElements()) {
+                NetworkInterface networkInterface = enumerationNi.nextElement();
+                String interfaceName = networkInterface.getDisplayName();
+//                Log.e("tag", "网络名字" + interfaceName);
+
+                // 如果是有限网卡
+                if (interfaceName.equals("eth0")) {
+                    Enumeration<InetAddress> enumIpAddr = networkInterface
+                            .getInetAddresses();
+
+                    while (enumIpAddr.hasMoreElements()) {
+                        // 返回枚举集合中的下一个IP地址信息
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        // 不是回环地址，并且是ipv4的地址
+                        if (!inetAddress.isLoopbackAddress()
+                                && inetAddress instanceof Inet4Address) {
+                            Log.e("tag", inetAddress.getHostAddress() + "   ");
+
+                            return inetAddress.getHostAddress();
+                        }
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+
+
+}
